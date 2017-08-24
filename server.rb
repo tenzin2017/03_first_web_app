dc2bb4fe25d4bc8eb831f67bb6f601a4fe862026
tenzin2017 <tenzinleo@gmail.com>
2017-08-24 15:50:11 -0400

    redirect route to home and portfolio is added

--- server.rb
+++ server.rb
@@ -1,5 +1,14 @@
 require 'sinatra'
 
+get '/' do
+  redirect to('/home')
+end
+
+get '/gallery' do
+
+  redirect to('/portfolio')
+end
+
 get '/home' do
   erb :index
 end
@@ -9,6 +18,14 @@ get '/portfolio' do
 end
 
 get '/about_me' do
-
+   @skills = ['git', 'HTML', 'CSS', 'Ruby']
+   @interests = ['Cats', 'art', 'music', 'films', 'coffee']
   erb :about_me
+end
+
+get '/favourites' do
+  @fav_links = ['https:www.dalailama.com', 'https:www.bitmaker.co', 'https:www.bbcworld.com', 'https:stackoverflow.com','http:www.codecademy.com']
+
+  erb :favourites
+
 end
